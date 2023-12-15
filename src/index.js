ac450dd3e5b310cf08678f04883a506c96d95d86
julialozmin <julialozmin@gmail.com>
2023-12-15 21:38:59 +0100

    Added the API integration

--- src/index.js
+++ src/index.js
@@ -1,12 +1,22 @@
-function displayResponse(event) {
-  event.preventDefault();
+function displayQuote(response) {
+  console.log(response);
   new Typewriter("#quote", {
-    strings:
-      "If you look at what you have in life, you'll always have more. If you look at what you don't have in life, you'll never have enough. -Oprah Winfrey",
+    strings: response.data.answer,
     autoStart: true,
     delay: 20,
   });
 }
 
+function generatequote(event) {
+  event.preventDefault();
+  let searchFieldInput = document.querySelector("#search-field");
+  let apiKey = "o214a6c6f6d2f53a6749b30tbf45c1ef";
+  let prompt = `Retrieve a quote that includes the word ${searchFieldInput.value}`;
+  let context =
+    "Be polite and provide a very short and accurate answer in HTML format, placing the author of the quote in a separated <div> with the class author";
+  let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+  axios.get(apiUrl).then(displayQuote);
+}
+
 let searchFormElement = document.querySelector("#search-form");
-searchFormElement.addEventListener("submit", displayResponse);
+searchFormElement.addEventListener("submit", generatequote);
