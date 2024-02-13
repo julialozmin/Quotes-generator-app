06b4f6a3fea533055a242ce06482109df6c96be2
julialozmin <julialozmin@gmail.com>
2024-02-13 19:21:25 +0100

    Refactored the script (variables)

--- src/index.js
+++ src/index.js
@@ -9,17 +9,17 @@ function displayQuote(response) {
 
 function generatequote(event) {
   event.preventDefault();
-  let searchFieldInput = document.querySelector("#search-field");
-  let apiKey = "o214a6c6f6d2f53a6749b30tbf45c1ef";
+  const searchFieldInput = document.querySelector("#search-field");
+  const apiKey = "o214a6c6f6d2f53a6749b30tbf45c1ef";
   let prompt = `Retrieve a quote that includes the word ${searchFieldInput.value}`;
   let context =
     "Be polite and provide a very short and accurate answer in HTML format, placing the author of the quote in a separated <div> with the class author";
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
-  let holdingMessage = document.querySelector("#quote");
+  const holdingMessage = document.querySelector("#quote");
   holdingMessage.innerHTML = `<div class="blink">Generating...</div>`;
 
   axios.get(apiUrl).then(displayQuote);
 }
 
-let searchFormElement = document.querySelector("#search-form");
+const searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", generatequote);
